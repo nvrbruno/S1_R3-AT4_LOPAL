2768026666cc6bce11f32b33ce09ef9cf88a0e43
unknown <brunonavarrodavi@gmail.com>
2025-05-13 12:32:24 -0300

    Exercício 1

--- Ex1.js
+++ Ex1.js
@@ -6,7 +6,6 @@ do {
     if (numero < 0 || isNaN(numero)) { //condição quando o número for menor que zero e se o usuário colocar uma letra ele não aceita
         alert("você não escreveu um número positivo");
     }
-
 } while (numero < 0 || isNaN(numero)); //função que enquanto o usuário não digitar um número positivo fica repetindo infintamente
 
 alert(`boa você digitou um numero positivo`);
