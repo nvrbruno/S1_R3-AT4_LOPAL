0c00534d39f56339dc982564f4f3240696ec013e
unknown <brunonavarrodavi@gmail.com>
2025-05-13 12:33:30 -0300

    Exercício 4

--- Ex4.js
+++ Ex4.js
@@ -4,7 +4,6 @@ let sobrenome = "";//variável que arazena o sobrenome dado pelo usuário
 do {
     primeiroNome = prompt("Digite seu primeiro nome:");
 } while (primeiroNome === ""); //função que pede pro usuario o prmeiro nome, se o usuario nao digitar nada o programa repete ate o usuario escrever
-
 do {
     sobrenome = prompt("Digite seu sobrenome:");
 } while (sobrenome === "");//função que pede pro usuario o sobrenome, se o usuario não digitar nada o programa repete ate o usuario escrever
